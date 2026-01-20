75ca1f47d739713dfabf0870495bbcf0dcb92dbf
agardik <markilexg@gmail.com>
2026-01-20 20:26:22 +0200

    modified:   Results/DoseCalculation.cxx

--- Results/DoseCalculation.cxx
+++ Results/DoseCalculation.cxx
@@ -75,10 +75,44 @@ public:
 
     // IMPORTANT: prevent ROOT ownership bugs
     TH1::AddDirectory(kFALSE);
-
+    // Create output ROOT file
     TString rootOutName =
         TString::Format("DoseResults_%s.root", histoutname.c_str());
     TFile *rootFile = new TFile(rootOutName, "RECREATE");
+    // Create output trees for alpha and Li7
+    TTree *alphaTree = new TTree("AlphaTree", "Alpha track properties");
+    TTree *li7Tree   = new TTree("Li7Tree",   "Li7 track properties");
+
+    Int_t    t_event;
+    Double_t t_edep;
+    Double_t t_trackLength;
+    Double_t t_cosTheta;
+    Double_t t_x0;
+    Double_t t_y0;
+    Double_t t_z0;
+    Double_t t_halpha;
+    Double_t t_hli7;
+
+    alphaTree->Branch("event", &t_event, "event/I");
+    alphaTree->Branch("totalEdep", &t_edep, "totalEdep/D");
+    alphaTree->Branch("trackLength", &t_trackLength, "trackLength/D");
+    alphaTree->Branch("cosTheta", &t_cosTheta, "cosTheta/D");
+    alphaTree->Branch("x0", &t_x0, "x0/D");
+    alphaTree->Branch("y0", &t_y0, "y0/D");
+    alphaTree->Branch("z0", &t_z0, "z0/D");
+    alphaTree->Branch("depth", &t_halpha, "halpha/D");
+
+    li7Tree->Branch("event", &t_event, "event/I");
+    li7Tree->Branch("totalEdep", &t_edep, "totalEdep/D");
+    li7Tree->Branch("trackLength", &t_trackLength, "trackLength/D");
+    li7Tree->Branch("cosTheta", &t_cosTheta, "cosTheta/D");
+    li7Tree->Branch("x0", &t_x0, "x0/D");
+    li7Tree->Branch("y0", &t_y0, "y0/D");
+    li7Tree->Branch("z0", &t_z0, "z0/D");
+    li7Tree->Branch("depth", &t_hli7, "hli7/D");
+
+
+    // Read all entries and store in vectors
 
     TreeReader::Analyze();
 
@@ -132,22 +166,44 @@ public:
     TH1F *Hist_StopTable =
         new TH1F("all_dep", "All Energy Deposition", 150, 0., 1.8);
     TH1F *Hist_StopFull =
-        new TH1F("Hist_StopFull", "Alpha interaction X", 100, 0., 4.);
+        new TH1F("Hist_StopFull", "Alpha interaction X", 100, 0., 35.);
     TH1F *DEDX_Hist =
-        new TH1F("DEDX_Hist", "dE/dx Alpha", 100, 0., 2.);
+        new TH1F("DEDX_Hist", "dE/dx Alpha", 100, 0., 6.);
     TH1F *Hist_StopPower =
         new TH1F("Hist_StopPower", "Stopping Power", 100, 0., 4.);
     TH1F *Hist_AlphaAngle =
     new TH1F("AlphaAngle",
              "Alpha angular distribution;cos(#theta);Counts",
-             50, -1.0, 1.0);
+             500, -1.0, 1.0);
 
-    
+    TH2F *Hist2D_Alpha_Edep_Cos =
+    new TH2F("Alpha_Edep_vs_CosTheta",
+             "Alpha: Total Edep vs cos(#theta);cos(#theta);Total Edep (MeV)",
+             100, -1.0, 1.0,   // cos(theta)
+             100,  0.0, 1.8);  // Edep (MeV)
+
+    TH2F *Hist2D_Li7_Edep_Cos =
+        new TH2F("Li7_Edep_vs_CosTheta",
+                "Li7: Total Edep vs cos(#theta);cos(#theta);Total Edep (MeV)",
+                100, -1.0, 1.0,
+                100,  0.0, 1.8);
+
+
+    // parameters for analysis
     Double_t edep_per_event = v_edepStep[0];
     Double_t StopPower_per_event = v_StopPower[0];
     bool have_first_step = false;
     bool have_first_alpha = true;
+    double li7_x0=0, li7_y0=0, li7_z0=0;
+    double li7_cosTheta = -999.0;
+    double halpha = -999.0;
+    double hli7 = -999.0;
+
+    bool have_first_li7_step  = false;
+    bool have_first_li7_track = true;
+
     double x0=0, y0=0, z0=0;
+    double alpha_cosTheta = -999.0;
     // ---- Alpha track selection parameters ----
     const double ALPHA_EDEP_MIN = 1.24;   // MeV (adjust)
     const double ALPHA_EDEP_MAX = 1.4;   // MeV (adjust)
@@ -163,6 +219,8 @@ public:
 
     // Temporary storage for current event
     std::vector<double> tmp_x, tmp_y, tmp_z;
+    std::vector<double> tmp_x_li7, tmp_y_li7, tmp_z_li7;
+
 
     // Storage for all selected alpha tracks for track length edep calculation
     std::vector<double> alpha_track_lengths;
@@ -176,11 +234,45 @@ public:
     // Loop over all entries
     for (size_t j = 1; j < v_evt.size(); j++) {
       if (v_evt[j] == v_evt[j - 1]) {
+        // ----- Li7 angular distribution -----
+        if (v_fParticleName[j] == "Li7" && have_first_li7_track) {
+
+          if (!have_first_li7_step) {
+            li7_x0 = v_posParticleX[j];
+            li7_y0 = v_posParticleY[j];
+            li7_z0 = v_posParticleZ[j];
+            have_first_li7_step = true;
+          }
+          else {
+            double dx = v_posParticleX[j] - li7_x0;
+            double dy = v_posParticleY[j] - li7_y0;
+            double dz = v_posParticleZ[j] - li7_z0;
+            double rholi7 = std::sqrt(li7_y0*li7_y0 + li7_z0*li7_z0);
+
+            have_first_li7_step  = false;
+            have_first_li7_track = false;
+
+            double norm = std::sqrt(dx*dx + dy*dy + dz*dz);
+            if (norm > 0) {
+              double theta = std::acos(dx / norm);
+              hli7=rholi7/std::tan(theta);
+              li7_cosTheta = dx / norm;
+            }
+          }
+        }
+
         // ---- Store alpha step positions ----
         if (v_fParticleName[j] == "alpha") {
           tmp_x.push_back(v_posParticleX[j]);
           tmp_y.push_back(v_posParticleY[j]);
           tmp_z.push_back(v_posParticleZ[j]);
+        }
+
+        // ---- Store Li7 step positions ----
+        if (v_fParticleName[j] == "Li7") {
+          tmp_x_li7.push_back(v_posParticleX[j]);
+          tmp_y_li7.push_back(v_posParticleY[j]);
+          tmp_z_li7.push_back(v_posParticleZ[j]);
         }
 
         // ----- Alpha angular distribution -----
@@ -198,11 +290,18 @@ public:
             double dx = v_posParticleX[j] - x0;
             double dy = v_posParticleY[j] - y0;
             double dz = v_posParticleZ[j] - z0;
+            double rhoalpha = std::sqrt(y0*y0 + z0*z0);
             have_first_step=false;
             double norm = std::sqrt(dx*dx + dy*dy + dz*dz);
             if (norm > 0) {
-              double theta = std::acos(dx / norm) * 180.0 / M_PI;
-              Hist_AlphaAngle->Fill(cos(theta*M_PI/180.0));
+              alpha_cosTheta = dx / norm;
+              // guard against cosTheta ≈ 0
+              if (std::abs(alpha_cosTheta) > 1e-6) {
+                  double sinTheta = std::sqrt(1.0 - alpha_cosTheta*alpha_cosTheta);
+                  halpha = rhoalpha * alpha_cosTheta / sinTheta;   // same as rho / tan(theta)
+              }
+              
+              Hist_AlphaAngle->Fill(alpha_cosTheta);
               // only once per alpha
               have_first_alpha = false;
             }
@@ -211,13 +310,18 @@ public:
         }
         edep_per_event += v_edepStep[j];
         StopPower_per_event += v_StopPower[j];
-        if (v_fParticleName[j] == "alpha") {
+        if (v_fParticleName[j] == "alpha" && edep_per_event>0.0001) {
           DEDX_Hist->Fill(v_MeandEdx[j]);
           Hist_StopFull->Fill(v_posParticleX[j]);
         }
       } else {
         have_first_alpha = true;
-        if (v_fParticleName[j - 1] == "alpha")
+        have_first_li7_track = true;
+        // New event encountered, process the previous event
+        // Fill histograms and trees based on particle type
+        if (v_fParticleName[j - 1] == "alpha" && edep_per_event>0.0001) {
+          Hist2D_Alpha_Edep_Cos->Fill(alpha_cosTheta, edep_per_event);
+          // Fill histograms for edep
           Hist_LET->Fill(edep_per_event);
               // Apply energy band cut
           if (edep_per_event >= ALPHA_EDEP_MIN &&
@@ -228,14 +332,16 @@ public:
             alpha_y.push_back(tmp_y);
             alpha_z.push_back(tmp_z);
           }
-        if (v_fParticleName[j - 1] == "Li7")
+        }
+        if (v_fParticleName[j - 1] == "Li7" && edep_per_event>0.0001){
+          Hist2D_Li7_Edep_Cos->Fill(li7_cosTheta, edep_per_event);
           Hist_TID->Fill(edep_per_event);
-
+        }
         Hist_StopTable->Fill(edep_per_event);
         Hist_StopPower->Fill(StopPower_per_event);
 
         // --- Compute track length for previous alpha event ---
-        if (v_fParticleName[j - 1] == "alpha" && tmp_x.size() > 1) {
+        if (v_fParticleName[j - 1] == "alpha" && tmp_x.size() > 1 && edep_per_event>0.0001) {
           double trackLength = 0.0;
           for (size_t k = 1; k < tmp_x.size(); k++) {
             double dx = tmp_x[k] - tmp_x[k - 1];
@@ -245,7 +351,42 @@ public:
           }
           alpha_track_lengths.push_back(trackLength);
           alpha_total_edep.push_back(edep_per_event);
+        
+          // Fill alpha tree
+          t_event = v_evt[j - 1];
+          t_edep = edep_per_event;
+          t_trackLength = trackLength;
+          t_cosTheta = alpha_cosTheta;
+          t_x0       = x0-1.3; // Adjust for detector position
+          t_y0       = y0;
+          t_z0       = z0;
+          t_halpha   = x0-halpha-1.3; // Adjust for detector position
+          alphaTree->Fill();
         }
+        // --- Compute track length for previous Li7 event ---
+        if (v_fParticleName[j - 1] == "Li7" && tmp_x_li7.size() > 1 && edep_per_event>0.0001) {
+
+          double trackLength = 0.0;
+          for (size_t k = 1; k < tmp_x_li7.size(); k++) {
+            double dx = tmp_x_li7[k] - tmp_x_li7[k - 1];
+            double dy = tmp_y_li7[k] - tmp_y_li7[k - 1];
+            double dz = tmp_z_li7[k] - tmp_z_li7[k - 1];
+            trackLength += std::sqrt(dx*dx + dy*dy + dz*dz);
+          }
+          // Fill Li7 tree
+          t_event       = v_evt[j - 1];
+          t_edep        = edep_per_event;
+          t_trackLength = trackLength;
+
+          t_cosTheta = li7_cosTheta;
+          t_x0       = li7_x0 - 1.3; // Adjust for detector position
+          t_y0       = li7_y0;
+          t_z0       = li7_z0;
+          t_hli7     = li7_x0 - hli7 - 1.3; // Adjust for detector position
+
+          li7Tree->Fill();
+        }
+
 
         if (v_fParticleName[j - 1] == "alpha" ||
             v_fParticleName[j - 1] == "Li7")
@@ -256,6 +397,9 @@ public:
         tmp_x.clear();
         tmp_y.clear();
         tmp_z.clear();
+        tmp_x_li7.clear();
+        tmp_y_li7.clear();
+        tmp_z_li7.clear();
       }
     }
 
@@ -267,6 +411,8 @@ public:
     Hist_StopPower->Write();
     DEDX_Hist->Write();
     Hist_AlphaAngle->Write();
+    Hist2D_Alpha_Edep_Cos->Write();
+    Hist2D_Li7_Edep_Cos->Write();
     // ---------- Plot selected alpha tracks 3d----------
     // ---------- Plot selected alpha tracks with axes ----------
     if (!alpha_x.empty()) {
@@ -422,7 +568,8 @@ public:
 
     // -----------------------------------------------
 
-
+    alphaTree->Write();
+    li7Tree->Write();
     rootFile->Close();
     delete rootFile;
 
